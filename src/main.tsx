77ae9a16f28d4cab763f72805132b2e56f5d4711
jonatas1n <jonatasgeek@gmail.com>
2025-02-26 02:52:30 -0300

    feat: apply theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { GlobalStyles } from "./styles/globalStyles.ts";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme.ts";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
