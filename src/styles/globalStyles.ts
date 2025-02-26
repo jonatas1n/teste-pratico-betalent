b27ac255fd9320aceb99898cec13081b171f1538
jonatas1n <jonatasgeek@gmail.com>
2025-02-26 14:53:24 -0300

    chore: set global style

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+  box-sizing: border-box;
+}
+
+body {
+    font-family: Helvetica;
+    margin: 0;
+    padding: 0;
+    background-color: ${({ theme }) => theme.colors.gray["0"]};
+    color: ${({ theme }) => theme.colors.text};
+  }
+`;
