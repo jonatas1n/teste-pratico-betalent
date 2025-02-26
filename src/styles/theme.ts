1895527b6cb3d18b69d87e986133377079405db7
jonatas1n <jonatasgeek@gmail.com>
2025-02-26 14:53:46 -0300

    chore: define theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    blue: {
+      primary: "#0500FF",
+      10: "#EDEFFB"
+    },
+    black: "#1C1C1C",
+    gray: {
+      0: "#F0F0F0",
+      5: "#F5F5F5",
+      10: "#DFDFDF",
+      20: "#9E9E9E"
+    },
+    white: "#FFFFFF",
+  }
+}
