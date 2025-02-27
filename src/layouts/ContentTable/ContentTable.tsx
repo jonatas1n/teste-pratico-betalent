1fb11fc1b6ee1e8c20f7aa80f889e626c301bea5
jonatas1n <jonatasgeek@gmail.com>
2025-02-27 13:33:45 -0300

    chore: adjust conditional display to content table

--- src/layouts/ContentTable/ContentTable.tsx
+++ src/layouts/ContentTable/ContentTable.tsx
@@ -3,24 +3,27 @@ import { Table } from "../../components/Table/Table";
 import { Spinner } from "../../components/Spinner";
 import useSWR from "swr";
 import { transformEmployeeType } from "./utils";
+import { FetchError } from "../../components/FetchError";
+import { EmployeeResponse } from "../../types";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = async (url: string): Promise<EmployeeResponse[]> => {
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Erro ao carregar os dados");
+  return res.json();
+};
 
 export const ContentTable = () => {
-  const { data, isLoading } = useSWR("/api/employees", fetcher);
+  const { data, isLoading, error } = useSWR("/api/employees", fetcher);
 
-  const headers = ["Foto", "Nome", "Cargo", "Data de Admissão", "Telefone"]
-  const employeesData =
-    data &&
-    data.map(transformEmployeeType);
+  const headers = ["Foto", "Nome", "Cargo", "Data de Admissão", "Telefone"];
+  const employeesData = data ? data.map(transformEmployeeType) : [];
+
+  if (isLoading) return <Spinner />;
+  if (error) return <FetchError />;
 
   return (
     <Container>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <Table headers={headers} data={employeesData} />
-      )}
+      <Table headers={headers} data={employeesData} />
     </Container>
   );
 };
