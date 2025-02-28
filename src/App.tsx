7fdc0d6ac8c428b2d77d42e954104c31f58b5f9c
jonatas1n <jonatasgeek@gmail.com>
2025-02-28 10:37:04 -0300

    chore: move location change to search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import { Header } from "./layouts/Header";
 import { TitleBar } from "./layouts/TitleBar/TitleBar";
-import { useState } from "react";
 import { Grid } from "./components/Grid";
-import { ContentTable } from "./layouts/ContentTable/ContentTable";
+import { ContentTable } from "./layouts/ContentTable";
 
 const TITLE_BAR_TEXT = "Funcionários";
 
 function App() {
-  const [searchValue, setSearchValue] = useState("");
-
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(event.target?.value);
-  };
-
   return (
     <Grid>
       <Header />
       <TitleBar
         title={TITLE_BAR_TEXT}
-        searchValue={searchValue}
-        setSearchValue={handleSearchChange}
       />
       <ContentTable />
     </Grid>
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,18 +1,36 @@
 import { IoIosSearch } from "react-icons/io";
-import { StyledSearch } from "./Search.styles"
+import { StyledSearch } from "./Search.styles";
+import { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
+import { useNavigate } from "react-router-dom";
+import { toSearchString } from "../../utils";
 
-type SearchProps = {
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  value: string;
-}
+export const Search = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
 
-export const Search = ({onChange, value}: SearchProps) => {
+  useDebounce(() => setDebouncedTerm(searchTerm), 300, [searchTerm]);
+
+  useEffect(() => {
+    const newSearch = toSearchString({ term: debouncedTerm });
+    navigate({ search: newSearch }, { replace: true });
+  }, [debouncedTerm, navigate]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <StyledSearch>
-      <input onChange={onChange} value={value} placeholder="Pesquisar" />
+      <input
+        onChange={handleSearchChange}
+        value={searchTerm}
+        placeholder="Pesquisar"
+      />
       <div className="icon">
         <IoIosSearch />
       </div>
     </StyledSearch>
-  )
-}
+  );
+};
