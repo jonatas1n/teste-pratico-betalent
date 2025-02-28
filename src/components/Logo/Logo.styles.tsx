6be40f47303377d53c2ec9833c21733b1dd3aefb
jonatas1n <jonatasgeek@gmail.com>
2025-02-28 12:52:22 -0300

    chore: change motion import

--- src/components/Logo/Logo.styles.tsx
+++ src/components/Logo/Logo.styles.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import * as motion from "motion/react-client";
 
-export const StyledLogo = styled.img`
+export const StyledLogo = styled(motion.img)`
   height: 0.875rem;
   width: auto;
 `
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,15 +1,14 @@
 import logoImg from "../../assets/Logo.svg";
 import { StyledLogo } from "./Logo.styles";
-import * as motion from "motion/react-client";
 
 export const Logo = () => {
   return (
-    <motion.div
+    <StyledLogo
+      src={logoImg}
       initial={{ opacity: 0, translateX: 16 }}
       animate={{ opacity: 1, translateX: 0 }}
       transition={{ duration: 0.75 }}
-    >
-      <StyledLogo src={logoImg} alt="Logo" />
-    </motion.div>
+      alt="Logo"
+    />
   );
 };
