0897e2e16f8525c1d876ae49ee165263f38c99e9
jonatas1n <jonatasgeek@gmail.com>
2025-02-28 12:50:34 -0300

    feat: animate logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,15 @@
 import logoImg from "../../assets/Logo.svg";
 import { StyledLogo } from "./Logo.styles";
+import * as motion from "motion/react-client";
 
 export const Logo = () => {
   return (
-    <StyledLogo src={logoImg} alt="Logo" />
-  )
-}
+    <motion.div
+      initial={{ opacity: 0, translateX: 16 }}
+      animate={{ opacity: 1, translateX: 0 }}
+      transition={{ duration: 0.75 }}
+    >
+      <StyledLogo src={logoImg} alt="Logo" />
+    </motion.div>
+  );
+};
