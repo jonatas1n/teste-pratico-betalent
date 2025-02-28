6ce7c285e9483ea1bc1ebeafa6fea825d71bf9cb
jonatas1n <jonatasgeek@gmail.com>
2025-02-28 13:00:28 -0300

    feat: animate search button

--- src/components/Search/SearchButton/SearchButton.tsx
+++ src/components/Search/SearchButton/SearchButton.tsx
@@ -1,15 +1,24 @@
 import { IoIosSearch } from "react-icons/io";
 import { StyledSearchClose } from "./SearchButton.styles";
+import * as motion from "motion/react-client";
 
 type SearchButtonProps = {
   onClick: VoidFunction;
   show: boolean;
-}
+};
 
-export const SearchButton = ({onClick, show}: SearchButtonProps) => {
+export const SearchButton = ({ onClick, show }: SearchButtonProps) => {
   return (
-    <>
-      {show ? <StyledSearchClose onClick={onClick} /> : <IoIosSearch className="icon"/>}
-    </>
-  )
+    <motion.div
+      initial={{ opacity: 0, scale: 0.25 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ delay: 0.25 }}
+    >
+      {show ? (
+        <StyledSearchClose onClick={onClick} />
+      ) : (
+        <IoIosSearch className="icon" />
+      )}
+    </motion.div>
+  );
 };
